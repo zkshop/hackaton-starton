1cf88d1c6a8b432b0150505eb452946c6dd8f336
Sinane <sin.sim@outlook.fr>
2022-11-27 14:37:34 +0100

    feat: last character is not a slash validation

--- app/form/constants.ts
+++ app/form/constants.ts
@@ -1,2 +1,3 @@
 export const REQUIRED = "This field is required.";
 export const NOT_A_NUMBER = "This field should be a number > 0.";
+export const LAST_CHARACTER_IS_NOT_SLASH = "Last character must be a '/'";
--- app/form/schema.ts
+++ app/form/schema.ts
@@ -1,10 +1,11 @@
 import * as yup from "yup";
 import { NOT_A_NUMBER, REQUIRED } from ".";
+import { LAST_CHARACTER_IS_NOT_SLASH } from "./constants";
 
 export const CONTRACT_BUILDER_SCHEMA = yup.object().shape({
   collectionName: yup.string().required(REQUIRED).min(0, NOT_A_NUMBER),
   symbol: yup.string().required(REQUIRED),
-  ipfsURI: yup.string().required(REQUIRED),
+  ipfsURI: yup.string().matches(/.*\/$/, LAST_CHARACTER_IS_NOT_SLASH).required(REQUIRED),
   maxSupply: yup.number().required(REQUIRED).min(0, NOT_A_NUMBER),
   ownerAddress: yup.string().required(REQUIRED),
   startonApiKey: yup.string().required(REQUIRED),
